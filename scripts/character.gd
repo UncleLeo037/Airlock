b3831d8059de2a5dadafaba4b4ffe65fe19ee473
UncleLeo037 <leo.muller@gmail.com>
2025-04-26 15:19:56 +0100

    RigidBody player

--- scripts/character.gd
+++ scripts/character.gd
@@ -1,14 +1,12 @@
 extends RigidBody3D
 
-
-var _pid := PidController.new(1.0, 0.1, 1.0)
-
 const SPEED = 5.0
 const JUMP_VELOCITY = 6.0
 
 @export var SENSITIVITY = 0.08
 
-@onready var camera = $Camera3D
+@onready var head : Node3D = $CollisionShape3D
+@onready var camera = $CollisionShape3D/Camera3D
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -16,24 +14,22 @@ func _ready() -> void:
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		rotate_y(deg_to_rad(-event.relative.x * SENSITIVITY))
+		head.rotate_y(deg_to_rad(-event.relative.x * SENSITIVITY))
 		camera.rotate_x(deg_to_rad(-event.relative.y * SENSITIVITY))
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
-
+func _physics_process(_delta: float) -> void:
 	# Handle jump.
-	#if Input.is_action_just_pressed("aloft") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("aloft"):
+		linear_velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("port", "starboard", "forward", "aftward")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	
-	var target_velocity = direction * SPEED
-	var velocity_error = target_velocity - linear_velocity
-	var correction_impulse = _pid.update(velocity_error, delta) * 0.05
+	var direction := (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		linear_velocity.x = direction.x * SPEED
+		linear_velocity.z = direction.z * SPEED
+	else:
+		linear_velocity.x = move_toward(linear_velocity.x, 0, SPEED)
+		linear_velocity.z = move_toward(linear_velocity.z, 0, SPEED)
