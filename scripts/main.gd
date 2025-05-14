4e61ff9703bb22a3cde2295aac7a513ba0e5deb3
UncleLeo037 <leo.muller@gmail.com>
2025-05-14 15:20:46 +0100

    Steam update
    basic steam functionality

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,8 +1,60 @@
-extends Node3D
+extends Node2D
 
-const Player = preload("res://scenes/player.tscn")
+@onready var ms = $MultiplayerSpawner
+
+var lobby_id = 0
+var peer = SteamMultiplayerPeer.new()
 
 func _ready() -> void:
-	var player = Player.instantiate()
-	player.name = str(multiplayer.get_unique_id())
-	add_child(player)
+	ms.spawn_function = spawn_level
+	peer.lobby_created.connect(_on_lobby_created)
+	Steam.lobby_match_list.connect(_on_lobby_match_list)
+	open_lobby_list()
+
+func spawn_level(map):
+	var a = (load(map) as PackedScene).instantiate()
+	return a
+
+func _on_host_pressed() -> void:
+	peer.create_lobby(SteamMultiplayerPeer.LOBBY_TYPE_PUBLIC)
+	multiplayer.multiplayer_peer = peer
+	ms.spawn("res://scenes/world.tscn")
+	$Host.hide()
+	$LobbyContainer/Lobbies.hide()
+	$Refresh.hide()
+
+func join_lobby(id):
+	peer.connect_lobby(id)
+	multiplayer.multiplayer_peer = peer
+	lobby_id = id
+	$Host.hide()
+	$LobbyContainer/Lobbies.hide()
+	$Refresh.hide()
+
+func _on_lobby_created(connect, id):
+	if connect:
+		lobby_id = id
+		Steam.setLobbyData(lobby_id, "name", str(Steam.getPersonaName()+"'s Lobby"))
+		Steam.setLobbyJoinable(lobby_id, true)
+		#print(lobby_id)
+
+func open_lobby_list():
+	Steam.addRequestLobbyListDistanceFilter(Steam.LOBBY_DISTANCE_FILTER_WORLDWIDE)
+	Steam.requestLobbyList()
+
+func _on_lobby_match_list(lobbies):
+	for lobby in lobbies:
+		var lobby_name = Steam.getLobbyData(lobby, "name")
+		var mem_count = Steam.getNumLobbyMembers(lobby)
+		
+		var but = Button.new()
+		but.set_text(str(lobby_name, "| Player Count: ", mem_count))
+		but.set_size(Vector2(100, 5))
+		but.connect("pressed", Callable(self, "join_lobby").bind(lobby))
+		
+		$LobbyContainer/Lobbies.add_child(but)
+
+func _on_refresh_pressed():
+	if $LobbyContainer/Lobbies.get_child_count() > 0:
+		for n in $LobbyContainer/Lobbies.get_children():
+			n.queue_free()
---
+++ scripts/spawner.gd
@@ -0,0 +1,23 @@
+extends MultiplayerSpawner
+
+@export var playerScene : PackedScene
+
+var players = {}
+
+func _ready() -> void:
+	spawn_function = spawnPlayer
+	if is_multiplayer_authority():
+		spawn(1)
+		multiplayer.peer_connected.connect(spawn)
+		multiplayer.peer_disconnected.connect(removePlayer)
+
+
+func spawnPlayer(data):
+	var p = playerScene.instantiate()
+	p.set_multiplayer_authority(data)
+	players[data] = p
+	return p
+
+func removePlayer(data):
+	players[data].queue_free()
+	players.erase(data)
